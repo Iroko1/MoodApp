af76ef5edbac15423f3c17cc56fac944f27f6280
darianhughes <darianmhughes7@gmail.com>
2025-04-21 08:26:16 -0500

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by ROTSEVENKOV, MATVEY V. on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
