0fdbe6d1e8a98f3bf6ea162e2a910fda2ce80f70
Ayden <ad0988877@otc.edu>
2025-04-21 08:56:57 -0500

    MoodPicker view

---
+++ MoodApp/MoodPicker.swift
@@ -0,0 +1,18 @@
+//
+//  MoodPicker.swift
+//  MoodApp
+//
+//  Created by DOWNING, AYDEN T. on 4/21/25.
+//
+
+import SwiftUI
+
+struct MoodPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodPicker()
+}
